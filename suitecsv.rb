6a81e43c2a22d9d3901bea32841ce6bb882b114d
Ben Di <bdifrancesco@ascensionpress.com>
2012-02-29 14:22:43 -0500

    Added initial support for merging- still missing row reorginization

--- suitecsv.rb
+++ suitecsv.rb
@@ -24,9 +24,67 @@ class MergeCSV < SuiteCSV
 	def initialize(filename, keys)
 		@keys = keys
 		super(filename)
+		
+		# Ensure the CSV has a header for each defined key
+		@keys.each do |key|
+			if not @headers.index(key)
+				raise "ERROR: could not find header for key: #{key}"
+		  	end
+		end
 	end
 	
+	def merge(other)
+		
+		# Ensure the other CSV has the same headers
+		@headers.each do |header|
+			if not other.headers.index(header)
+				return false
+			end
+		end
+		
+		# Go through each row in the other CSV
+		other.matrix.each do |other_row|
+		
+			# init the var to track whether this row is already present
+			already_present = false
+			
+			# Go through each row in myself, see if the other's row is here
+			@matrix.each do |my_row|
+			
+				# If the keys match this row is present - stop checking
+				if keys_match?(my_row, other_row, other.headers)
+					already_present = true
+					break
+				end
+			end
+			
+			# Add this row to th
+			if not already_present
+				#@matrix.push other_row
+				$stdout.puts "Adding row: #{other_row}"
+			end
+		end
+
+	end
 	
+	# Do the keys from my_row match the key from other_row
+	def keys_match?(my_row, other_row, other_headers)
+		@keys.each do |key|
+			# Get each index of this key
+			my_index = @headers.index(key)
+			other_index = other_headers.index(key)
+			
+			# If each value at this key doesn't match, return false
+			if not my_row[my_index] == other_row[other_index]
+				return false
+			end
+		end
+		
+		# If we checked values at each key w/o mismatch, its the same
+		return true
+	end
+	
+	private :keys_match?
 end
 
 # Take two CSVs and produce a result that is the overlap of
@@ -41,10 +99,13 @@ class JoinCSV < SuiteCSV
 	end
 end
 
-sample1 = SuiteCSV.new("sample1.csv")
+sample1 = MergeCSV.new("sample1.csv", ["internal id", "last name"])
+sample2 = SuiteCSV.new("sample2.csv")
 
-puts sample1.headers
+sample1.merge sample2
 
-sample1.matrix.each do |row|
-	puts row
-end
+#puts sample1.headers
+
+#sample1.matrix.each do |row|
+#	puts row
+#end
