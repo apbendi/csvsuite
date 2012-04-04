e2790150a0351b702639862f98e160ea7a4ee6db
Ben Di <bdifrancesco@ascensionpress.com>
2012-04-04 15:22:34 -0400

    Fix & finish Merge tests & include CSV's needed, closes #3

--- test/tc_testMergeCSV.rb
+++ test/tc_testMergeCSV.rb
@@ -34,22 +34,23 @@ class TestMergeCSV < Test::Unit::TestCase
 	def test_merge2
 		sample1 = MergeCSV.new "sample1.csv", ["Internal ID", "Last Name"]
 		full 	= MergeCSV.new "us_presidents_full.csv", ["Internal ID", "Last Name"]
+		full.headers<< "From Sample" # Add this so we don't cause a merge error for mismatched cols
 
 		# If the source CSV lacks columns in the destination, there should be an error
 		assert_raise(RuntimeError) { full.merge sample1 }
 
 		assert_nothing_raised() do
 			sample1.merge full
-			sample1.write "sample1-full_merge_mer.csv"
+			sample1.write "sample1-full_merge.csv"
 		end
 
 		# Initialize a CVS versions of the answer & our result
-		csv_1_full_merge 	= CSV.new File.new("sample1-full_merge_mer.csv"), {:headers => true}
+		csv_1_full_merge 	= CSV.new File.new("sample1-full_merge.csv"), {:headers => true}
 		table_1_full_merge 	= csv_1_full_merge.read
 		csv_1_full_ans 		= CSV.new File.new("sample1-full_merge_ans.csv"), {:headers => true}
 		table_1_full_ans 	= csv_1_full_ans.read
-
+  
 		# Ensure our results match the known, correct answer
-		assert_equal csv_1_full_ans, csv_1_full_merge
+		assert_equal table_1_full_ans, table_1_full_merge, "Merge did not produce expected results"
 	end
 end
